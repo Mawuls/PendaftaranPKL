8c6cb1924a7317e0fdacb3fbc0749515d20a4f64
Mawuls <Mawuls>
2025-06-14 10:25:39 +0700

    Add project files.

---
+++ PendaftaranPKL/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PendaftaranPKL.Models;
+
+namespace PendaftaranPKL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
